e1bbbc34c34985b7cb0429851b4289e1992c13b6
kzhaburdenok <katerina.zhaburdenok@gmail.com>
2022-04-27 17:39:41 +0300

    rewrite test in pageObject (almost)

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,10 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/"
-
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()     
+   
 
 def test_guest_can_go_to_login_page(browser):
-
-    browser.get(link)
-    go_to_login_page(browser)
+    page = MainPage(browser,link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open() # открываем страницу - это у нас задано в base_page
+    page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина - это у нас задано в main_page
