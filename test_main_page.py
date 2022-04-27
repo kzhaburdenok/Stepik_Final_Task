140f669a40b072b06805fcc40f21028734c31c86
kzhaburdenok <katerina.zhaburdenok@gmail.com>
2022-04-27 18:43:03 +0300

    exception added

--- test_main_page.py
+++ test_main_page.py
@@ -7,4 +7,5 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser,link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open() # открываем страницу - это у нас задано в base_page
-    page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина - это у нас задано в main_page
+    page.should_be_login_link()
+    #page.go_to_login_page() # выполняем метод страницы — переходим на страницу логина - это у нас задано в main_page
