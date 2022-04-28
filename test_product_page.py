26d5dd5df543f0a2528f014d2a7dbcf6b3da2502
kzhaburdenok <katerina.zhaburdenok@gmail.com>
2022-04-28 18:32:40 +0300

    removed skipped marks

--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,6 @@ import pytest
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
 
-@pytest.mark.skip
 @pytest.mark.parametrize("promo_offer", [pytest.param(i, marks=pytest.mark.xfail(i=="{i}", reason="")) for i in range (10)])
 def test_guest_can_add_product_to_basket(browser, promo_offer):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
@@ -14,7 +13,6 @@ def test_guest_can_add_product_to_basket(browser, promo_offer):
     page.get_product_name()
     page.get_product_price()
 
-@pytest.mark.skip
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
@@ -23,14 +21,12 @@ def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     page.add_product_to_cart()
     page.should_not_be_success_msg()
 
-@pytest.mark.skip
 def test_guest_cant_see_success_message(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.should_not_be_success_msg()
 
-@pytest.mark.skip
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
@@ -38,13 +34,13 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_product_to_cart()
     page.should_disappear()
-@pytest.mark.skip
+
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
-@pytest.mark.skip
+
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "https://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
