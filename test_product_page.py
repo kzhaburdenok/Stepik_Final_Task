ef38e37614102ed2456f15725bd32b3e35858dec
kzhaburdenok <katerina.zhaburdenok@gmail.com>
2022-04-28 16:22:41 +0300

    parametrized

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,12 @@
+import pytest
 from .pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize("promo_offer", [pytest.param(i, marks=pytest.mark.xfail(i=="{i}", reason="")) for i in range (10)])
+def test_guest_can_add_product_to_basket(browser, promo_offer):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo_offer}"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
-    page.product_should_be_added_to_cart()
+    page.get_product_name()
+    page.get_product_price()
